262a9a30452aec73fea732885784e7080f2d1bac
ariannadepasquale <depasqualearianna@gmail.com>
2022-09-01 13:55:02 +0200

    Add test Poster Film&Series

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -7,9 +7,12 @@
                         <ul>
                             <li>FILM</li>                            
                             <li v-for="(film,index) in filmList" :key="index">
+                                <!-- Poster -->
+                                <img :src="'https://image.tmdb.org/t/p/w92' + film.poster_path " :alt="'Poster for' + film.title" >
                                 {{film.title}}-
                                 {{film.original_title}}-
                                 {{film.original_language}}-
+                                <!-- Flags -->
                                 <img class="flags" v-if="avaibleFlags.includes(film.original_language)"
                                 :src="require('../assets/images/'+ film.original_language + '.png')">
                                 <span v-else>
@@ -20,19 +23,22 @@
                         </ul>
                         </div>
                         <div class="col text-white">
-                    <!-- SERIES FILM -->
+                    <!-- CARD SERIES-->
                         <ul>
                             <li>SERIES</li>                            
                             <li v-for="(series,index) in seriesList" :key="index">
+                                <!-- Poster -->
+                                <img :src="'https://image.tmdb.org/t/p/w92' + series.poster_path " :alt="'Poster for' + series.name" >
                                 {{series.name}}-
                                 {{series.original_name}}-
                                 {{series.original_language}}-
+                                <!-- Flags -->
                                 <img class="flags" v-if="avaibleFlags.includes(series.original_language)"
                                 :src="require('../assets/images/'+ series.original_language + '.png')">
                                 {{series.vote_average}}-
                             </li>                    
                         </ul>
-<img src="" alt="">
+
 
                         </div>
                 </div>
